ddeb3982c37df17cd2b81d60097574f61ff67614
Nitish <167279871+NitishGupta1107@users.noreply.github.com>
2026-07-25 06:18:17 +0000

    Scope level and hoisting in js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,15 +1,57 @@
-// var c = 300;
+// // var c = 300;
 
-let a = 300;
+
+// let a = 300;
+// if(true){
+//  let a = 10;
+// const b = 20;
+// console.log(a);
+// }
+
+
+
+// console.log(a);
+
+// // console.log(c);
+
+
+// function one (){
+//     const username = "Nitish"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+
+//     // console.log(website); // This will generate a error
+//     two();
+// }
+
+// one();
+
+
 if(true){
- let a = 10;
-const b = 20;
-console.log(a);
+    const username = "Nitish"
+    if(username === "Nitish"){
+        const website = " youtube"
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// Interesting conceptt
+
+addOne(5);
+function addOne(num){
+    return num + 1;
 }
 
 
 
-console.log(a);
-
-// console.log(c);
-
+addTwo(5)
+const addTwo = function(num){
+   return num + 2;
+}
